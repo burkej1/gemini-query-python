4b9ea0b2ffbbb145a977d6c2eb9506c297ba96fd
burkej1 <burkej1@student.unimelb.edu.au>
2018-02-15 15:44:35 +1100

    First commit

---
+++ classes.py
@@ -0,0 +1,52 @@
+# Contains some useful classes
+
+class Options(object):
+    """A container to hold various default values and incorporate any values passed as arguments."""
+    def __init__(self):
+        self.fields = ["gene", 
+                       "impact", 
+                       "is_lof", 
+                       "chrom", 
+                       "start", 
+                       "end", 
+                       "ref", 
+                       "alt", 
+                       "filter", 
+                       "qual_depth", 
+                       "vep_hgvsc", 
+                       "vep_hgvsp", 
+                       "vep_brcaex_hgvs_cdna", 
+                       "vep_brcaex_hgvs_protein", 
+                       "vep_brcaex_clinical_significance_enigma", 
+                       "vep_brcaex_date_last_evaluated_enigma", 
+                       "num_het", 
+                       "num_hom_alt"]
+        self.where_filters = ["vep_pick = 1 AND filter = None"]
+        self.transcripts = ["BRCA1:NM_007294.3",  # BRCA1 transcript (vep gets this one wrong). Format like this
+                            "BRCA2:NM_000059.3"]
+
+    def update_with_arguments(self, args):
+        """Updates internal values based on passed arguments"""
+        pass
+
+    def translate_where(self, plaintext_where):
+        """Translates plain text arguments to specific where queries"""
+        # Formatting more complex where queries here
+
+        # # Primary transcripts passing filters and variants in requested transcripts passing filters
+        # Exclude the primary annotation blocks for genes where transcripts where specified
+        genes_to_exclude = [transcript.split(':')[0] for transcript in self.transcripts]
+        genes_to_exclude = " AND ".join(["gene != " + gene for gene in genes_to_exclude])
+
+        transcripts_to_include = [transcript.split(':')[1] for transcript in self.transcripts]
+        standard_transcripts = "(vep_pick = 1 AND filter = None AND ( {exclude} )) OR " \
+                               "(filter = None AND {transcriptlist}".format(transcriptlist=self.transcripts, 
+                                                                            exclude=genes_to_exclude, 
+                                                                            include=transcripts_to_include)
+
+        # Instantiating the dictionary
+        translation_dictionary = {"standard": "vep_pick = 1 AND filter = None"
+                                  "standard_transcripts": standard_transcripts}
+
+        # Translating the request
+        return translation_dictionary[plaintext_where]
---
+++ main.py
@@ -0,0 +1,59 @@
+from __future__ import print_function
+from gemini import GeminiQuery  # Importing the gemini query class
+import argparse  # For parsing arguments
+import classes
+
+
+def print_fields(db):
+    """Returns all fields in the given database"""
+    query = "SELECT * FROM variants limit 1"
+    db.run(query)
+    return db.header
+
+
+def get_table(db, args):
+    """Returns a table of variants based on the fields and filter options provided"""
+    pass
+
+
+def main():
+    """Main function which parses arguments and calls relevant functions"""
+    # Pulling various default options and setting up the options class
+    options = classes.Options()
+
+    # Defining the argument parser
+    parser = argparse.ArgumentParser(description="Python wrapper for interacting with GEMINI databases.")
+    parser.add_argument("mode", help="Mode to run in. " \
+                                     "sample - returns all variants in a given sample; " \
+                                     "variant - search for a given variant; " \
+                                     "table - returns a table of all variants with sample lists filtered " \
+                                     "using given criteria; " \
+                                     "info - print the fields present in the database.")
+    parser.add_argument("-i", "--input", help="Input database to query.", required=True)
+    parser.add_argument("-o", "--output", help="File to write query results to.", required=True)
+    parser.add_argument("-f", "--fields", help="List of fields to pull.", default=options.fields)
+    parser.add_argument("-w", "--where", help="List of filters in SQL WHERE structure.", 
+                        default=options.where_filters)
+    # Parsing the arguments and storing as a dictionary
+    arguments = vars(parser.parse_args())
+
+    # Updating the program options based on the passed arguments
+    options.update_with_arguments(arguments)
+
+    # Creating the gemini database object from the input database
+    gemini_db = GeminiQuery.GeminiQuery(arguments["input"])
+
+    # Calling relevant function depending on the mode option
+    if arguments["mode"] == "sample":
+        pass
+    elif arguments["mode"] == "variant":
+        pass
+    elif arguments["mode"] == "table":
+        pass
+    elif arguments["mode"] == "info":
+        print_comprehension = [print(field) for field in print_fields(gemini_db).split('\t')]
+
+
+if __name__ == "__main__":
+    main()
+
