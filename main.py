41b8660d23f14c2a998675807d08d2599e01fdb2
burkej1 <burkej1@student.unimelb.edu.au>
2018-02-19 14:17:03 +1100

    Interim commit

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from __future__ import print_function
 from gemini import GeminiQuery  # Importing the gemini query class
 import argparse  # For parsing arguments
+import re
 import classes
 
 
-def print_fields(db):
+def get_fields(db):
     """Returns all fields in the given database"""
     query = "SELECT * FROM variants limit 1"
     db.run(query)
@@ -25,6 +26,51 @@ def get_table(db, args, options):
     return table_lines
 
 
+def get_sample_variants(db, args, options):
+    """Returns a table of variants present in a given sample (by BSID or full sample name)"""
+    sampleid = args["sampleid"]
+    if re.match("BS\d\d\d\d\d\d", sampleid):
+        # If a BSID is given find the corresponding full name in the database
+        print("Searching for BSID.")
+        idquery = "SELECT name FROM samples WHERE name LIKE '%{bsid}%'".format(bsid=sampleid)
+        db.run(idquery)
+        matches = []
+        for row in db:
+            matches.append(str(row))
+        if len(matches) > 1:
+            print("Multiple matches for given BSID, exiting.")
+            quit()
+        elif len(matches) == 0:
+            print("No matches found for given BSID, exiting.")
+            quit()
+        else:
+            print("Found: {match}".format(match=matches[0]))
+        fullsampleid = matches[0]
+        gt_filter = "gt_types.{fullsampleid} == HET OR " \
+                    "gt_types.{fullsampleid} == HOM_ALT".format(fullsampleid=fullsampleid)
+    else:
+        # If a BSID is not given it's assumed the full name was given
+        fullsampleid = sampleid
+        gt_filter = "gt_types.{fullsampleid} == HET OR " \
+                    "gt_types.{fullsampleid} == HOM_ALT".format(fullsampleid=fullsampleid)
+    genotype_information = "gts.{fsi}, gt_ref_depths.{fsi}, gt_alt_depths.{fsi}, gt_alt_freqs.{fsi}" \
+                               .format(fsi=fullsampleid)
+    query = "SELECT {fields}, {genotypeinfo} FROM variants WHERE {where_filter}" \
+                .format(fields=', '.join(options.fields), 
+                                         where_filter=options.where_filters, 
+                                         genotypeinfo=genotype_information)
+    print("Generating a table from the following query filtered to only include variants present in the " \
+          "given sample:")
+    print(query)
+    print(gt_filter)
+    # # Recreating the gemini database object to clear the previous query
+    # db = GeminiQuery.GeminiQuery(args["input"])  
+    db.run(query, gt_filter, show_variant_samples=False)
+    table_lines = [str(db.header)]
+    for row in db:
+        table_lines.append(str(row))
+    return table_lines
+
 
 def main():
     """Main function which parses arguments and calls relevant functions"""
@@ -39,10 +85,13 @@ def main():
     parser.add_argument("-i", "--input", help="Input database to query.", required=True)
     parser.add_argument("-o", "--output", help="File to write query results to.", required=True)
     parser.add_argument("-sf", "--simple_filter", help="Preset filter options.", default=None)
+    parser.add_argument("-S", "--sampleid", help="Sample ID to use when running in sample mode. Can be " \
+                                                 "supplied as either BSID only or as a file name.", default=None)
     # Below are more manual options that will override defaults
     parser.add_argument("-f", "--fields", help="Comma separated list of fields to pull.", default=None)
     parser.add_argument("-w", "--where", help="Filter string in SQL WHERE structure.", default=None)
     # Need to add a an argument for transcript lists, not sure whether to take a file or string as input #
+    # Might also want to add an argument that will take a list of samples/BSIDs
     arguments = vars(parser.parse_args()) # Parsing the arguments and storing as a dictionary
 
     options = classes.Options()  # Setting up the options class with a number of defaults
@@ -52,7 +101,9 @@ def main():
 
     # Calling relevant function depending on the chosen mode
     if arguments["mode"] == "sample":
-        pass
+        output_table = get_sample_variants(gemini_db, arguments, options)
+        with open(arguments["output"], 'w') as outputfile:
+            outputfile.write('\n'.join(output_table))
     elif arguments["mode"] == "variant":
         pass
     elif arguments["mode"] == "table":
@@ -60,7 +111,7 @@ def main():
         with open(arguments["output"], 'w') as outputfile:
             outputfile.write('\n'.join(output_table))
     elif arguments["mode"] == "info":
-        print_comprehension = [print(field) for field in print_fields(gemini_db).split('\t')]
+        print_comprehension = [print(field) for field in get_fields(gemini_db).split('\t')]
 
 
 if __name__ == "__main__":
